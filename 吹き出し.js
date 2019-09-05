1148a503922aa1578341b85c47f1f0db39e9d7af
薗部匡矢 <masaya.tanks07@gmail.com>
2019-09-06 08:43:17 +0900

    あのんちゃんが８月に重複させていたプログラムを削除するコミットをします。

--- 吹き出し.js
+++
@@ -1,8 +0,0 @@
-function anon1(){
-    var risako=document.getElementById("hukihuki");
-    risako.style.display="block";
-}
-function anon2(){
-    var risako2=document.getElementById("hukihuki");
-    risako2.style.display="none";
-}
