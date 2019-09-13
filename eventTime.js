28be858d612f54adc815c65911761eef02737ffe
薗部匡矢 <masaya.tanks07@gmail.com>
2019-09-13 11:17:10 +0900

    eventTime.htmlとeventTime.jsを使った時間帯別イベント表示機能を吹き出し中に表示させるようにしました。 また、イベント出演者の画像は、2019年度の方々に切り替えてます。

--- eventTime.js
+++ eventTime.js
@@ -1,19 +1,41 @@
+//時間帯別表示js
 var now_hour=new Date().getHours();
 var now_minute=new Date().getMinutes();
 
+var evewin = document.getElementById('eventWindow');
+
 switch(now_hour){
-    case 2:
+    case 5:
+        console.log("case 4 was used.");
+
         if(now_minute>=0 && now_minute<30){
-            document.write('<img src="eventImg/和牛.jpg">');
+            evewin.innerHTML='<img src="eventImg/フレンズ.png">';
         }
         else{
-            document.write('<img src="eventImg/藤崎マーケット.jpg">');
+            evewin.innerHTML='<br><img src="eventImg/ハンブレッダーズ.png">';
         }
         break;
-    case 17:
-        document.write('<img src="eventImg/mainartist2019-300x253.png">');
-        break;
-    default:
-        document.write('<div>イベント当日までお待ちください。<br><img src="eventImg/申し訳ございません.jpg"></div>');
+    case 6:
+        evewin.innerHTML='<img src="eventImg/mainartist2019-300x253.png"><br>They are フレンズ.';
         break;
 }
+//時間帯別表示js
+
+//吹き出しjs
+$(function(){
+    //フェードイン
+    var cnt=0;
+    $('#button').on('click', function() {
+        console.log("clicked.");
+        $('#eventWindow').fadeIn(300);
+        cnt++;
+    })
+    //フェードアウト
+    window.addEventListener('click', function(){
+        if(cnt%2===0){
+            console.log("clicked.");
+            $('#eventWindow').fadeOut(300); 
+        }
+    },false);
+});
+//吹き出しjs
